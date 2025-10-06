ec22677f4fe08be55003d4e2f1d03bff54724b95
aikoshh <comunicazionestrategicaroma@gmail.com>
2025-10-06 14:37:19 +0200

    Add files via upload

--- assets.ts
+++ assets.ts
@@ -3,5 +3,5 @@
 
 export const cardImage1 = 'https://images.unsplash.com/photo-1556761175-5973dc0f32e7?q=80&w=600&h=360&auto=format&fit=crop';
 export const cardImage2 = 'https://images.unsplash.com/photo-1543269865-cbf427effbad?q=80&w=600&h=360&auto=format&fit=crop';
-export const cardImage3 = 'https://images.unsplash.com/photo-1573496130141-209d200ce478?q=80&w=600&h=360&auto=format&fit=crop';
+export const cardImage3 = 'https://images.unsplash.com/photo-1516321497487-e288fb19713f?q=80&w=600&h=360&auto=format&fit=crop';
 export const cardImage4 = 'https://images.unsplash.com/photo-1484480974693-6ca0a78fb36b?q=80&w=600&h=360&auto=format&fit=crop';
