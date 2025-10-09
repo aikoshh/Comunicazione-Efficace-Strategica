c1b68257091dfedb21a4ff730e18159cd6a50135
aikoshh <comunicazionestrategicaroma@gmail.com>
2025-10-09 14:03:02 +0200

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import React from 'react';
+import { ErrorBoundary } from './components/ErrorBoundary';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
+
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
