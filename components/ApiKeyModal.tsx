074b4a3f6c85a0adcbef6c5a29c7727a644df51e
aikoshh <comunicazionestrategicaroma@gmail.com>
2025-10-17 18:23:57 +0200

    Delete components directory

--- components/ApiKeyModal.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// This component is currently not used in the main application flow,
-// but is being provided as a valid component to replace placeholder content.
-const ApiKeyModal: React.FC = () => {
-  return null; // Render nothing
-};
-
-export default ApiKeyModal;
